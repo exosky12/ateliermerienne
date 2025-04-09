d62235acc262d4d16c2efd1b57d60de031bd6af3
exosky12 <exoskyv2@gmail.com>
2025-04-09 08:43:38 +0200

    refactor: conventions

--- inertia/layouts/AppLayout.tsx
+++ inertia/layouts/AppLayout.tsx
@@ -1,11 +1,11 @@
 import { Navbar } from '~/components/navbar/Navbar'
 
-export const AppLayout = ({ Children }: { Children: React.ReactNode }) => {
+export const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
 
-      <main className={'grow min-h-screen'}>{Children}</main>
+      <main className={'grow min-h-screen'}>{children}</main>
 
       <footer>
         <div className="flex justify-center items-center h-16 bg-gray-800 text-white">
