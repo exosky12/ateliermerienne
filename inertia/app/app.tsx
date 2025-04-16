a141bd89441e2c9d6fe276dfcd7babe63f015d68
exosky12 <exoskyv2@gmail.com>
2025-04-16 08:49:35 +0200

    fix: persistent layout

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -4,27 +4,29 @@
 import '../css/app.css'
 import { hydrateRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react'
+
+import { Layout } from '~/components/layout/app'
+import type { ComponentType } from 'react'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
-import { AppLayout } from '~/layouts/AppLayout'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Atelier Merienne'
 
-createInertiaApp({
+void createInertiaApp({
   progress: { color: '#5468FF' },
 
   title: (title) => (title ? `${title} - ${appName}` : appName),
 
   resolve: async (name) => {
-    const page = await resolvePageComponent(
+    const page = (await resolvePageComponent(
       `../pages/${name}.tsx`,
       import.meta.glob('../pages/**/*.tsx')
-    ) as { default: React.ComponentType<any> }
+    )) as { default: ComponentType<any> }
 
     const Page = page.default
     const WrappedPage = (props: any) => (
-      <AppLayout>
+      <Layout>
         <Page {...props} />
-      </AppLayout>
+      </Layout>
     )
 
     return { default: WrappedPage }
--- inertia/components/layout/app.tsx
+++ inertia/components/layout/app.tsx
@@ -1,16 +1,12 @@
-import { Navbar } from '~/components/navbar/Navbar'
+import { NavBar } from '~/components/navBar/navBar'
+import type { ReactNode } from 'react'
 
-type AppLayoutProps = {
-  children: React.ReactNode
-}
-
-export const AppLayout = ({ children }: AppLayoutProps) => {
+export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={'flex flex-col w-full min-h-screen'}>
-      <Navbar />
+      <NavBar />
 
       <main className={'grow min-h-screen'}>{children}</main>
-
       <footer>
         <div className="flex justify-center items-center h-16 bg-gray-800 text-white">
           <p>&copy; 2025 Atelier Merienne. Tous droits réservés.</p>
