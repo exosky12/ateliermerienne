ef340f7de0e4b8ec40b8d0c05bcb169d78813ddc
exosky12 <exoskyv2@gmail.com>
2025-05-12 23:15:42 +0200

    style(Squircle): make each button squircle

--- inertia/components/layout/app.tsx
+++ inertia/components/layout/app.tsx
@@ -1,19 +1,29 @@
 import { NavBar } from '~/components/navBar/navBar'
-import type { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
+import { addCorners } from '@monokai/monoco'
+import { Footer } from '~/components/footer/footer'
 
 export function Layout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const btns = document.querySelectorAll('.btn') as NodeListOf<HTMLElement>
+    btns.forEach((btn) =>
+      addCorners(btn, {
+        smoothing: 1,
+        borderRadius: 18,
+        clip: true,
+      })
+    )
+  }, [])
   return (
     <div
-      className={'flex text-lg flex-col w-full min-h-screen bg-background text-primary font-sans'}
+      className={
+        'flex text-[16px] flex-col w-full min-h-screen bg-background text-primary font-sans'
+      }
     >
       <NavBar />
 
       <main className={'grow min-h-screen'}>{children}</main>
-      <footer>
-        <div className="flex justify-center items-center h-16 bg-gray-800 text-white">
-          <p>&copy; 2025 Atelier Merienne. Tous droits réservés.</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
