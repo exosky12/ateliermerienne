19233584e73117141bf7904b1c04e96deb71a587
exosky12 <exoskyv2@gmail.com>
2025-05-12 23:11:28 +0200

    feat(HomePage): get current user logged id and show his email

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,10 +1,15 @@
 import { Head } from '@inertiajs/react'
+import { usePage } from '@inertiajs/react'
+import User from '#models/user'
 
 export default function Home() {
+  const { props } = usePage()
+  const user: User = props.user ?? {}
   return (
     <>
       <Head title="Accueil" />
 
+      <h2>{user?.email}</h2>
       <h2>
         Découvrez Sweety, notre sac tonneau au design élégant. Modulable grâce à sa bandoulière
         amovible, il se porte à la main ou à l’épaule. Son cuir raffiné se décline en plusieurs
