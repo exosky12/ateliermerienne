8e99d78703963f0522a2df120c0a982cf1377b41
exosky12 <exoskyv2@gmail.com>
2025-05-12 22:46:58 +0200

    feat(routes): auth routes (get & post for connexion, inscription and logout. Use for guest and auth middlewares to protect auth routes)

--- start/routes.ts
+++ start/routes.ts
@@ -7,7 +7,24 @@
 |
 */
 
+import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
+const LogoutController = () => import('#controllers/logout_controller')
+const LoginController = () => import('#controllers/login_controller')
+const RegisterController = () => import('#controllers/register_controller')
 const HomeController = () => import('#controllers/home_controller')
 
 router.get('/', [HomeController, 'index']).as('home.index')
+
+router
+  .group(() => {
+    router.get('/connexion', [LoginController, 'render']).as('login.index')
+    router.post('/connexion', [LoginController, 'store']).as('login.store')
+    router.get('/inscription', [RegisterController, 'render']).as('register.index')
+    router.post('/inscription', [RegisterController, 'store']).as('register.store')
+  })
+  .use(middleware.guest())
+  .as('auth')
+
+router.get('/logout', [LogoutController, 'render']).as('logout.render').use(middleware.auth())
+router.post('/logout', [LogoutController, 'execute']).as('logout.execute').use(middleware.auth())
