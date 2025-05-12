145f766a543f94a64fa06a1e71b1445d9015c7af
exosky12 <exoskyv2@gmail.com>
2025-05-12 23:12:50 +0200

    style(NavBar): redesign

--- inertia/components/navBar/mobileNav.tsx
+++ inertia/components/navBar/mobileNav.tsx
@@ -15,17 +15,21 @@ export const MobileNav = ({ links }: MobileNavBarProps) => {
   }
 
   return (
-    <div className="xl:hidden">
+    <div className="xl:hidden order-first">
       <button
         onClick={handleClick}
         className="p-2"
         aria-label={isOpen ? 'Fermer menu' : 'Ouvrir menu'}
       >
-        {isOpen ? <X strokeWidth={1} /> : <Menu strokeWidth={1} />}
+        {isOpen ? (
+          <X strokeWidth={2} color={'#4C3225'} />
+        ) : (
+          <Menu strokeWidth={2} color={'#4C3225'} />
+        )}
       </button>
 
       {isOpen && (
-        <div className="absolute top-full left-0 right-0 bg-white z-50 border-b border-black py-4 px-6">
+        <div className="absolute top-full left-0 right-0 bg-background z-50 border-b-2 border-primary py-4 px-6">
           <ul className="flex flex-col gap-4">
             {links.map((link: NavLink) => (
               <li key={link.href}>
