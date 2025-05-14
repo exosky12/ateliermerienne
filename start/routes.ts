61d7a4b8b0ea6f98320f130852b29a2f3830ec13
exosky12 <exoskyv2@gmail.com>
2025-05-14 23:47:31 +0200

    feat(routes): manage OAuth routes

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,7 @@
 
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
-const LogoutController = () => import('#controllers/logout_controller')
+const AuthController = () => import('#controllers/auth_controller')
 const LoginController = () => import('#controllers/login_controller')
 const RegisterController = () => import('#controllers/register_controller')
 const HomeController = () => import('#controllers/home_controller')
@@ -18,13 +18,23 @@ router.get('/', [HomeController, 'render']).as('home.index').use(middleware.sile
 
 router
   .group(() => {
-    router.get('/connexion', [LoginController, 'render']).as('login.index')
-    router.post('/connexion', [LoginController, 'store']).as('login.store')
-    router.get('/inscription', [RegisterController, 'render']).as('register.index')
-    router.post('/inscription', [RegisterController, 'store']).as('register.store')
+    router.get('/auth/connexion', [LoginController, 'render']).as('login.index')
+    router.post('/auth/connexion', [LoginController, 'store']).as('login.store')
+
+    router.get('/auth/inscription', [RegisterController, 'render']).as('register.index')
+    router.post('/auth/inscription', [RegisterController, 'store']).as('register.store')
+
+    router.get('/auth/google', [AuthController, 'google']).as('auth.google')
+    router
+      .get('/auth/google/callback', [AuthController, 'googleCallback'])
+      .as('auth.googleCallback')
+
+    router.get('/auth/facebook', [AuthController, 'facebook']).as('auth.facebook')
+    router
+      .get('/auth//facebook/callback', [AuthController, 'facebookCallback'])
+      .as('auth.facebookCallback')
   })
   .use(middleware.guest())
   .as('auth')
 
-router.get('/logout', [LogoutController, 'render']).as('logout.render').use(middleware.auth())
-router.post('/logout', [LogoutController, 'execute']).as('logout.execute').use(middleware.auth())
+router.get('/auth/logout', [AuthController, 'logout']).as('logout.execute').use(middleware.auth())
