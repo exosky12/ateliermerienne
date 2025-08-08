2a803cbbce0aac5bede8cf1f353259bcbfe6439d
exosky12 <exoskyv2@gmail.com>
2025-08-08 17:59:37 +0200

    feat(config): update lucide-react version and enhance font settings

--- uno.config.ts
+++ uno.config.ts
@@ -2,11 +2,24 @@ import { defineConfig, presetWebFonts, presetWind4 } from 'unocss'
 
 export default defineConfig({
   theme: {
+    letterSpacing: {
+      title: '-0.04em',
+      geist: '-0.02em',
+    },
+    fontFamily: {
+      title: 'title',
+      sans: 'sans',
+    },
     colors: {
       primary: '#4C3225',
       background: '#F2ECE9',
     },
   },
+
+  shortcuts: [
+    ['font-title', 'font-title tracking-title'],
+    ['font-geist', 'font-sans tracking-geist'],
+  ],
 
   presets: [
     presetWind4({
@@ -15,14 +28,14 @@ export default defineConfig({
       },
     }),
     presetWebFonts({
-      themeKey: 'font',
-      provider: 'bunny',
+      themeKey: 'fontFamily',
       fonts: {
-        sans: 'Outfit',
+        sans: 'Geist',
         title: [
           {
             name: 'Playfair Display',
             weights: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+            italic: true,
           },
         ],
       },
