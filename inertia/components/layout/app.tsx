1ff6b8cc23bb8cb4f30f46e1624cee2088cb64b7
exosky12 <exoskyv2@gmail.com>
2025-08-12 23:01:36 +0200

    feat(ui): update button styles for social links and remove unused corner styling

--- inertia/components/layout/app.tsx
+++ inertia/components/layout/app.tsx
@@ -1,19 +1,19 @@
 import { NavBar } from '~/components/navBar/navBar'
-import { ReactNode, useEffect } from 'react'
-import { addCorners } from '@monokai/monoco'
+import { ReactNode } from 'react'
+// import { addCorners } from '@monokai/monoco'
 import { Footer } from '~/components/footer/footer'
 
 export function Layout({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    const btns = document.querySelectorAll('.btn') as NodeListOf<HTMLElement>
-    btns.forEach((btn) =>
-      addCorners(btn, {
-        smoothing: 1,
-        borderRadius: 16,
-        clip: true,
-      })
-    )
-  }, [])
+  // useEffect(() => {
+  //   const btns = document.querySelectorAll('.btn') as NodeListOf<HTMLElement>
+  //   btns.forEach((btn) =>
+  //     addCorners(btn, {
+  //       smoothing: 1,
+  //       borderRadius: 16,
+  //       clip: true,
+  //     })
+  //   )
+  // }, [])
   return (
     <div
       className={
