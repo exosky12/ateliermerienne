24f6fd1b34dc9819a61559cb968925e6f6218b4c
exosky12 <exoskyv2@gmail.com>
2025-08-12 23:01:19 +0200

    feat(ui): refactor registration page layout and replace input fields with reusable component

--- inertia/pages/login/index.tsx
+++ inertia/pages/login/index.tsx
@@ -1,6 +1,7 @@
 import { Head, Link, usePage } from '@inertiajs/react'
 import { Socials } from '~/components/socials/socials'
 import { Button } from '~/components/button/button'
+import { Input } from '~/components/input/input'
 
 interface LoginProps {
   csrfToken: string
@@ -12,7 +13,7 @@ export default function Login({ csrfToken }: LoginProps) {
   return (
     <div className={'flex h-screen w-full'}>
       <Head title="Connexion" />
-      <div className={'w-full m-8 md:m-0 md:w-1/2 h-screen'}>
+      <div className={'w-full m-8 md:m-0  md:w-1/2 h-screen'}>
         <div
           className={
             'flex w-auto max-w-lg h-auto flex-col gap-8 items-left m-auto mt-18 md:ml-[15%] md:mt-[15%]'
@@ -45,13 +46,7 @@ export default function Login({ csrfToken }: LoginProps) {
                 <label htmlFor="email" className="text-sm ">
                   Email
                 </label>
-                <input
-                  id="email"
-                  type="email"
-                  name={'email'}
-                  placeholder="atelier@merienne.fr"
-                  className="rounded-2xl border-strokeGrey p-4"
-                />
+                <Input type={'email'} placeholder={'atelier@merienne.fr'} />
                 {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
               </div>
 
@@ -65,13 +60,8 @@ export default function Login({ csrfToken }: LoginProps) {
                     Mot de passe oublié ?
                   </Link>
                 </div>
-                <input
-                  id="password"
-                  type="password"
-                  name={'password'}
-                  placeholder="********"
-                  className="rounded-2xl  border-strokeGrey p-4 "
-                />
+
+                <Input type={'password'} placeholder={'********'} />
                 {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
               </div>
             </div>
@@ -88,78 +78,11 @@ export default function Login({ csrfToken }: LoginProps) {
           </span>
         </div>
       </div>
-      <div className="hidden relative w-1/2 h-screen md:flex">
-        <div className="absolute inset-6 bg-accent rounded-[33px] flex items-center justify-center">
-          <img src="/rose.png" alt="Rose" className="max-h-[80%]" />
+      <div className="hidden relative w-1/2 h-5/6 mt-12 md:flex">
+        <div className="absolute inset-6 rounded-[33px] overflow-hidden">
+          <img src="/authImage.png" alt="Rose" className="w-full h-full object-cover" />
         </div>
       </div>
     </div>
-    // <div className="flex h-screen">
-    //   <div className="flex flex-col z-10 flex-1 justify-center m-auto max-w-screen sm:max-w-2/3 items-center gap-12 px-2 sm:px-8">
-    //     <h2 className="font-title font-bold text-5xl">Se connecter</h2>
-    //
-    //     <Socials />
-    //
-    //     <div className="flex items-center justify-center w-full gap-4">
-    //       <div className="h-0.5 w-full bg-primary"></div>
-    //       <p className="whitespace-nowrap">OU ALORS</p>
-    //       <div className="h-0.5 w-full bg-primary"></div>
-    //     </div>
-    //
-    //     <form
-    //       className="flex items-center w-full flex-col gap-4"
-    //       method="POST"
-    //       action={'/connexion'}
-    //       suppressHydrationWarning
-    //     >
-    //       <input type="hidden" name="_csrf" value={csrfToken} />
-    //       <div className="w-full">
-    //         <div>
-    //           <label htmlFor="email" className="text-lg">
-    //             Email
-    //           </label>
-    //           <input
-    //             id="email"
-    //             type="email"
-    //             name={'email'}
-    //             placeholder="atelier@merienne.fr"
-    //             className="rounded-xl border-primary border-2 p-4 w-full"
-    //           />
-    //           {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
-    //         </div>
-    //
-    //         <div>
-    //           <label htmlFor="password" className="text-lg">
-    //             Mot de passe
-    //           </label>
-    //           <input
-    //             id="password"
-    //             type="password"
-    //             name={'password'}
-    //             placeholder="********"
-    //             className="rounded-xl border-primary border-2 p-4 w-full"
-    //           />
-    //           <div className={'flex flex-col'}>
-    //             {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
-    //             <Link href={'/'} className={'underline'}>
-    //               Mot de passe oublié ?
-    //             </Link>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       {errors.E_INVALID_CREDENTIALS && (
-    //         <span className="text-red-500 text-sm">{errors.E_INVALID_CREDENTIALS}</span>
-    //       )}
-    //       <Button type={'submit'} text="Se connecter" />
-    //     </form>
-    //
-    //     <span className={'text-center'}>
-    //       Vous n'avez pas encore de compte ?{' '}
-    //       <Link className={'underline'} href={'/auth/inscription'}>
-    //         Inscrivez-vous
-    //       </Link>{' '}
-    //     </span>
-    //   </div>
-    // </div>
   )
 }
