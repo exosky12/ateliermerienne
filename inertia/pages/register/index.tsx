637ebd9aa0c85dc21377ebdbb2db131f090ba6a8
exosky12 <exoskyv2@gmail.com>
2025-08-12 23:01:19 +0200

    feat(ui): refactor registration page layout and replace input fields with reusable component

--- inertia/pages/register/index.tsx
+++ inertia/pages/register/index.tsx
@@ -1,6 +1,7 @@
 import { Head, Link, usePage } from '@inertiajs/react'
 import { Socials } from '~/components/socials/socials'
 import { Button } from '~/components/button/button'
+import { Input } from '~/components/input/input'
 
 interface RegisterProps {
   csrfToken: string
@@ -9,78 +10,76 @@ interface RegisterProps {
 export default function Register({ csrfToken }: RegisterProps) {
   const { props } = usePage()
   const errors = props.errors ?? {}
+
   return (
-    <div className="flex h-screen">
+    <div className={'flex h-screen w-full'}>
       <Head title="Inscription" />
-      <div className="flex flex-col flex-1 justify-center xl:max-w-full m-auto w-full sm:max-w-2/3 items-center gap-12 px-2 sm:px-8">
-        <h2 className="font-title font-bold text-5xl">S'inscrire</h2>
-
-        <Socials />
-
-        <div className="flex items-center justify-center w-full gap-4">
-          <div className="h-0.5 w-full bg-primary"></div>
-          <p className="whitespace-nowrap">OU ALORS</p>
-          <div className="h-0.5 w-full bg-primary"></div>
-        </div>
-
-        <form
-          className="flex items-center w-full flex-col gap-4"
-          method="POST"
-          action={'/auth/inscription'}
-          suppressHydrationWarning
+      <div className={'w-full m-8 md:m-0 md:w-1/2 h-screen'}>
+        <div
+          className={
+            'flex w-auto max-w-lg h-auto flex-col gap-8 items-left m-auto mt-18 md:ml-[15%] md:mt-[10%]'
+          }
         >
-          <input type="hidden" name="_csrf" value={csrfToken} />
-          <div className={'w-full'}>
-            <label htmlFor="email" className="text-lg">
-              Email
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="atelier@merienne.fr"
-              className="rounded-xl border-primary border-2 p-4 w-full"
-            />
-            {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
+          <div className={'flex flex-col gap-2'}>
+            <h1 className={'font-title font-italic text-[32px]'}>Créer un compte</h1>
+            <p className={'text-base'}>
+              Rejoignez notre espace pour suivre vos commandes, enregistrer vos préférences et
+              profiter de tous nos services.
+            </p>
           </div>
+          <Socials />
+          <div className={'w-full flex gap-2 justify-between items-center'}>
+            <div className={'bg-strokeGrey w-full h-0.5'}></div>
+            <span className={'text-textGrey text-xs'}>ou</span>
+            <div className={'bg-strokeGrey w-full h-0.5'}></div>
+          </div>
+          <form
+            className="flex items-center w-full flex-col gap-6"
+            method="POST"
+            action={'/auth/inscription'}
+            suppressHydrationWarning
+          >
+            <input type="hidden" name="_csrf" value={csrfToken} />
+            <div className="w-full flex flex-col gap-4">
+              <div className={'flex flex-col gap-1'}>
+                <label htmlFor="email" className="text-sm">
+                  Email
+                </label>
+                <Input type={'email'} placeholder={'atelier@merienne.fr'} />
+                {errors.email && <span className="text-red-500 text-sm">{errors.email}</span>}
+              </div>
+              <div className={'flex flex-col gap-1'}>
+                <label htmlFor="password" className="text-sm">
+                  Mot de passe
+                </label>
+                <Input type={'password'} placeholder={'********'} />
+                {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
+              </div>
+              <div className={'flex flex-col gap-1'}>
+                <label htmlFor="confirmPassword" className="text-sm">
+                  Confirmer le mot de passe
+                </label>
 
-          <div className={'w-full'}>
-            <label htmlFor="password" className="text-lg">
-              Mot de passe
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="********"
-              className="rounded-xl border-primary border-2 p-4 w-full"
-            />
-            {errors.password && <span className="text-red-500 text-sm">{errors.password}</span>}
-          </div>
-
-          <div className={'w-full'}>
-            <label htmlFor="confirmPassword" className="text-lg">
-              Confirmer le de passe
-            </label>
-            <input
-              id="confirmPassword"
-              name="confirmPassword"
-              type="password"
-              placeholder="********"
-              className="rounded-xl border-primary border-2 p-4 w-full"
-            />
-            {errors.confirmPassword && (
-              <span className="text-red-500 text-sm">{errors.confirmPassword}</span>
-            )}
-          </div>
-          <Button type={'submit'} text="S'inscire" />
-        </form>
-        <span className={'text-center'}>
-          Vous avez déjà un compte ?{' '}
-          <Link className={'underline'} href={'/auth/connexion'}>
-            Connectez-vous
-          </Link>{' '}
-        </span>
+                <Input type={'password'} id={'confirmPassword'} placeholder={'********'} />
+                {errors.confirmPassword && (
+                  <span className="text-red-500 text-sm">{errors.confirmPassword}</span>
+                )}
+              </div>
+            </div>
+            <Button type={'submit'} text="S'inscrire" />
+          </form>
+          <span className={'text-sm'}>
+            Vous avez déjà un compte ?{' '}
+            <Link className={'underline text-accent'} href={'/auth/connexion'}>
+              Connectez-vous
+            </Link>
+          </span>
+        </div>
+      </div>
+      <div className="hidden relative w-1/2 h-5/6 mt-12 md:flex">
+        <div className="absolute inset-6 rounded-[33px] overflow-hidden">
+          <img src="/authImage.png" alt="Rose" className="w-full h-full object-cover" />
+        </div>
       </div>
     </div>
   )
