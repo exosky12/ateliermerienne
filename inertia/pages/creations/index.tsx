bab89f7b4d0c89d5ed6c9472e26bc9c159c97d92
jumerienne <jules.merienne@etu.uca.fr>
2025-11-26 17:33:03 +0100

    feat: update creations pages and ui components

--- inertia/pages/creations/index.tsx
+++ inertia/pages/creations/index.tsx
@@ -2,18 +2,18 @@ import Product from "#models/product";
 import ProductsList from "~/components/productsList";
 
 interface CreationsProps {
-  products: Product[]
+    products: Product[]
 }
 
 export default function Creations({ products }: CreationsProps) {
     return (
         <div>
             <h1>Creations</h1>
-            
+
             <ProductsList products={products} />
 
         </div>
     )
-    
+
 
 }
