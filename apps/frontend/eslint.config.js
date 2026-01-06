f7f36f0e0400db7263336400fbc3744ff3b169a5
jumerienne <jules.merienne@etu.uca.fr>
2026-01-06 22:52:04 +0100

    feat: Add Husky and lint-staged for pre-commit hooks.

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,8 +1,29 @@
-//  @ts-check
-
-import { tanstackConfig } from '@tanstack/eslint-config'
 import { baseEslintConfig } from '@packages/config/eslint'
+// @ts-check
+import { tanstackConfig } from '@tanstack/eslint-config'
 
 const config = await baseEslintConfig()
 
-export default [...config, ...tanstackConfig]
+const basePlugins = new Set()
+config.forEach((c) => {
+	if (c.plugins) {
+		Object.keys(c.plugins).forEach((p) => basePlugins.add(p))
+	}
+})
+
+const cleanTanstackConfig = tanstackConfig.map((c) => {
+	if (!c.plugins) return c
+
+	const entries = Object.entries(c.plugins).filter(([key]) => !basePlugins.has(key))
+
+	if (entries.length === Object.keys(c.plugins).length) {
+		return c
+	}
+
+	return {
+		...c,
+		plugins: Object.fromEntries(entries),
+	}
+})
+
+export default [...config, ...cleanTanstackConfig]
