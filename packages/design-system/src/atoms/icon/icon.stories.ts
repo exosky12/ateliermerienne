054e427b016fef16dc0f10a486c115d13755a77b
jumerienne <jules.merienne@etu.uca.fr>
2026-01-08 14:49:29 +0100

    refactor: clean files

--- packages/design-system/src/atoms/icon/icon.stories.ts
+++ packages/design-system/src/atoms/icon/icon.stories.ts
@@ -15,8 +15,16 @@ export const Base: Story = {
 		name: {
 			options: IconName,
 		},
+		width: {
+			options: [16, 24, 32, 48, 64],
+		},
+		height: {
+			options: [16, 24, 32, 48, 64],
+		},
 	},
 	args: {
 		name: "search",
+		width: 24,
+		height: 24,
 	},
 };
