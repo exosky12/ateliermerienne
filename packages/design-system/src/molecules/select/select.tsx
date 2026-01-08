39b40833c3c2090c47792ac812295971d53a8c37
jumerienne <jules.merienne@etu.uca.fr>
2026-01-08 17:08:19 +0100

    style: refine select component styling

--- packages/design-system/src/molecules/select/select.tsx
+++ packages/design-system/src/molecules/select/select.tsx
@@ -24,20 +24,31 @@ export const Select = ({
 }: SelectProps) => {
 	return (
 		<BaseSelect.Root items={selectItems} defaultValue={defaultSelected}>
-			<BaseSelect.Trigger className="flex items-center gap-1.5 cursor-pointer text-sm">
+			<BaseSelect.Trigger className="group flex items-center gap-1.5 cursor-pointer text-sm">
 				<BaseSelect.Value />
 				<BaseSelect.Icon>
-					<Icon width={12} height={12} name="chevron-down" />
+					<Icon
+						width={12}
+						height={12}
+						name="chevron-down"
+						className="transition-transform duration-200 group-data-[popup-open]:rotate-180"
+					/>
 				</BaseSelect.Icon>
 			</BaseSelect.Trigger>
 			<BaseSelect.Portal>
-				<BaseSelect.Positioner className="outline-none" sideOffset={8}>
-					<BaseSelect.Popup className="origin-[var(--transform-origin)] rounded-md bg-[canvas] py-1 text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300 bg-white min-w-[120px]">
+				<BaseSelect.Positioner
+					className="outline-none z-50"
+					sideOffset={8}
+					side="bottom"
+					align="start"
+					alignItemWithTrigger={false}
+				>
+					<BaseSelect.Popup className="origin-[var(--transform-origin)] border bg-white overflow-hidden py-1 transition-[transform,scale,opacity] duration-200 ease-out data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300 min-w-[120px]">
 						{selectItems.map((item) => (
 							<BaseSelect.Item
 								key={item.value}
 								value={item.value}
-								className="flex cursor-pointer hover:underline py-2 pr-4 pl-4 text-sm leading-4 outline-none select-none justify-between items-center"
+								className="flex cursor-pointer hover:underline py-2 pr-4 pl-4 text-sm outline-none select-none justify-between items-center"
 							>
 								<BaseSelect.ItemText>{item.label}</BaseSelect.ItemText>
 								<BaseSelect.ItemIndicator>
