e5e2ba81e134fb27f71b70f0a701b924b4748481
jumerienne <jules.merienne@etu.uca.fr>
2026-02-19 22:53:53 +0100

    feat: implement Login and Signup pages

--- apps/frontend/src/routes/connexion.tsx
+++ apps/frontend/src/routes/connexion.tsx
@@ -1,8 +1,9 @@
+import { Image } from '@unpic/react'
 import { Field } from '@packages/design-system/field'
 import { useQueryClient } from '@tanstack/react-query'
-import { Link, createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute, useNavigate } from '@tanstack/react-router'
 
-import { tuyau } from '@/config/tuyau'
+import { tuyau } from '@/lib/tuyau.ts'
 import { DynamicForm } from '@/components/DynamicForm'
 
 export const Route = createFileRoute('/connexion')({
@@ -11,64 +12,71 @@ export const Route = createFileRoute('/connexion')({
 
 function RouteComponent() {
 	const queryClient = useQueryClient()
+	const navigate = useNavigate()
 
 	return (
-		<DynamicForm
-			defaultValues={{
-				email: '',
-				password: '',
-			}}
-			mutationFn={async (value) => {
-				const { data, response } = await tuyau.connexion.$post(value)
-				console.log(response)
-				return { data }
-			}}
-			onSuccess={async (data) => {
-				console.log('Success:', data)
-				await queryClient.invalidateQueries({ queryKey: ['isConnected'] })
-			}}
-			buttonLabel="Se connecter"
-		>
-			{(form) => (
-				<>
-					<form.Field
-						name="email"
-						children={(field: any) => (
-							<Field
-								type="email"
-								label="Email"
-								placeholder="Entrez votre email"
-								name={field.name}
-								value={field.state.value}
-								onBlur={field.handleBlur}
-								onChange={(e) => field.handleChange(e.target.value)}
-								errorMessage={field.state.meta.errors.join(', ')}
+		<div className="flex h-full w-full items-center">
+			<div className="w-1/2">
+				<DynamicForm
+					defaultValues={{
+						email: '',
+						password: '',
+					}}
+					mutationFn={async (value) => {
+						const { data } = await tuyau.connexion.$post(value)
+						return { data }
+					}}
+					onSuccess={async () => {
+						await queryClient.invalidateQueries({ queryKey: ['isConnected'] })
+						navigate({ to: '/profile' })
+					}}
+					buttonLabel="Se connecter"
+				>
+					{(form) => (
+						<>
+							<form.Field
+								name="email"
+								children={(field: any) => (
+									<Field
+										type="email"
+										label="Email"
+										placeholder="Entrez votre email"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										errorMessage={field.state.meta.errors.join(', ')}
+									/>
+								)}
 							/>
-						)}
-					/>
-					<form.Field
-						name="password"
-						children={(field: any) => (
-							<Field
-								type="password"
-								label="Mot de passe"
-								placeholder="Entrez votre mot de passe"
-								name={field.name}
-								value={field.state.value}
-								onBlur={field.handleBlur}
-								onChange={(e) => field.handleChange(e.target.value)}
-								errorMessage={field.state.meta.errors.join(', ')}
+							<form.Field
+								name="password"
+								children={(field: any) => (
+									<Field
+										type="password"
+										label="Mot de passe"
+										placeholder="Entrez votre mot de passe"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										errorMessage={field.state.meta.errors.join(', ')}
+									/>
+								)}
 							/>
-						)}
-					/>
-					<span>
-						Vous n'avez pas encore de compte ?{' '}
-						<Link className="underline" to="/inscription">
-							Inscrivez-vous
-						</Link>
-					</span>
-				</>
-			)}
-		</DynamicForm>
+							<span>
+								Vous n'avez pas encore de compte ?{' '}
+								<Link className="underline" to="/inscription">
+									Inscrivez-vous
+								</Link>
+							</span>
+						</>
+					)}
+				</DynamicForm>
+			</div>
+			<div className="w-1/2">
+				<Image alt="Photo d'une fauteuil" src="landing_card2.png" width={1920} height={1080} />
+			</div>
+		</div>
 	)
 }
--- apps/frontend/src/routes/inscription.tsx
+++ apps/frontend/src/routes/inscription.tsx
@@ -1,8 +1,9 @@
+import { Image } from '@unpic/react'
 import { Field } from '@packages/design-system/field'
 import { useQueryClient } from '@tanstack/react-query'
-import { Link, createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute, useNavigate } from '@tanstack/react-router'
 
-import { tuyau } from '@/config/tuyau'
+import { tuyau } from '@/lib/tuyau.ts'
 import { DynamicForm } from '@/components/DynamicForm'
 
 export const Route = createFileRoute('/inscription')({
@@ -11,78 +12,87 @@ export const Route = createFileRoute('/inscription')({
 
 function RouteComponent() {
 	const queryClient = useQueryClient()
+	const navigate = useNavigate()
 
 	return (
-		<DynamicForm
-			defaultValues={{
-				email: '',
-				password: '',
-				fullName: '',
-			}}
-			mutationFn={async (value) => {
-				await tuyau.inscription.$post(value)
-			}}
-			onSuccess={async () => {
-				await queryClient.invalidateQueries({ queryKey: ['isConnected'] })
-			}}
-			buttonLabel="S'inscrire"
-		>
-			{(form) => (
-				<>
-					<form.Field
-						name="fullName"
-						children={(field: any) => (
-							<Field
-								type="text"
-								label="Nom complet"
-								placeholder="Entrez votre nom complet"
-								name={field.name}
-								value={field.state.value}
-								onBlur={field.handleBlur}
-								onChange={(e) => field.handleChange(e.target.value)}
-								errorMessage={field.state.meta.errors.join(', ')}
+		<div className="flex h-full w-full items-center">
+			<div className="w-1/2">
+				<DynamicForm
+					defaultValues={{
+						email: '',
+						password: '',
+						fullName: '',
+					}}
+					mutationFn={async (value) => {
+						await tuyau.sign_up.$post(value)
+					}}
+					onSuccess={async () => {
+						await queryClient.invalidateQueries({ queryKey: ['user'] })
+						navigate({ to: '/profile' })
+					}}
+					buttonLabel="S'inscrire"
+				>
+					{(form) => (
+						<>
+							<form.Field
+								name="fullName"
+								children={(field: any) => (
+									<Field
+										type="text"
+										label="Nom complet"
+										placeholder="Entrez votre nom complet"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										errorMessage={field.state.meta.errors.join(', ')}
+									/>
+								)}
 							/>
-						)}
-					/>
-					<form.Field
-						name="email"
-						children={(field: any) => (
-							<Field
-								type="email"
-								label="Email"
-								placeholder="Entrez votre email"
-								name={field.name}
-								value={field.state.value}
-								onBlur={field.handleBlur}
-								onChange={(e) => field.handleChange(e.target.value)}
-								errorMessage={field.state.meta.errors.join(', ')}
+							<form.Field
+								name="email"
+								children={(field: any) => (
+									<Field
+										type="email"
+										label="Email"
+										placeholder="Entrez votre email"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										errorMessage={field.state.meta.errors.join(', ')}
+									/>
+								)}
 							/>
-						)}
-					/>
-					<form.Field
-						name="password"
-						children={(field: any) => (
-							<Field
-								type="password"
-								label="Mot de passe"
-								placeholder="Entrez votre mot de passe"
-								name={field.name}
-								value={field.state.value}
-								onBlur={field.handleBlur}
-								onChange={(e) => field.handleChange(e.target.value)}
-								errorMessage={field.state.meta.errors.join(', ')}
+							<form.Field
+								name="password"
+								children={(field: any) => (
+									<Field
+										type="password"
+										label="Mot de passe"
+										placeholder="Entrez votre mot de passe"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										errorMessage={field.state.meta.errors.join(', ')}
+									/>
+								)}
 							/>
-						)}
-					/>
 
-					<span>
-						Vous avez déjà un compte ?{' '}
-						<Link className="underline" to="/connexion">
-							Connectez-vous
-						</Link>
-					</span>
-				</>
-			)}
-		</DynamicForm>
+							<span>
+								Vous avez déjà un compte ?{' '}
+								<Link className="underline" to="/connexion">
+									Connectez-vous
+								</Link>
+							</span>
+						</>
+					)}
+				</DynamicForm>
+			</div>
+			<div className="w-1/2">
+				<Image alt="Photo d'une fauteuil" src="landing_card2.png" width={1920} height={1080} />
+			</div>
+		</div>
 	)
 }
